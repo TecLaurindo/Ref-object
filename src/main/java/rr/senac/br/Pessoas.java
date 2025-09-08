f6af502dae78e253f146bcb6b1bb4eeb51c2db19
Vinicius.Laurindo <tecnicolaurindo@gmail.com>
2025-09-08 13:31:53 -0400

    primeiro commit

---
+++ src/main/java/rr/senac/br/Pessoas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package rr.senac.br;
+
+/**
+ *
+ * @author devlaurindex
+ */
+public class Pessoas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
