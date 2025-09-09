238e2277cf7a579739b51576c39aaea292cfd955
Vinicius.Laurindo <tecnicolaurindo@gmail.com>
2025-09-08 21:05:08 -0400

    nova entrada no people

--- src/main/java/rr/senac/br/Pessoas.java
+++ src/main/java/rr/senac/br/Pessoas.java
@@ -9,9 +9,19 @@ public class Pessoas {
         P1.setIdade(18);
         P1.setCpf("999.999.999.999");
 
+        System.out.println("O que a pessoa deve fala?");
+        String fala = leia.nextLine();
+        P1.setFala(fala);
+
+        System.out.println("Quantos passos ele vai dar?");
+        int passo = leia.nextInt();
+        P1.setPasso(passo);
+
+
         P1.getNome();
         P1.getIdade();
         P1.getCpf();
-        
+        P1.getFala();
+        P1.getPasso();
     }
 }
--- src/main/java/rr/senac/br/people.java
+++ src/main/java/rr/senac/br/people.java
@@ -4,6 +4,8 @@ public class people {
     public String nome;
     public int idade;
     private String cpf;
+    public String Fala;
+    public int Passo;
     
     public String getNome(){
         System.out.print("Seu nome é " + this.nome);
@@ -15,7 +17,7 @@ public class people {
     }
 
     public int getIdade(){
-        System.out.println("\ne a sua idade é " + this.idade );
+        System.out.print("\ne a sua idade é " + this.idade );
         return this.idade;
     }
 
@@ -27,9 +29,27 @@ public class people {
         System.out.print("\ne o seu CPF é esse " + this.cpf);
         return this.cpf;
     }
+
         public void setCpf(String cpf){
         this.cpf = cpf;
     
     }
+
+    public String getFala(){
+        System.out.print("\nPowerRaner black piano argumentou " );
+        return this.Fala;
+    }
+
+        public void setFala(String Fala){
+            this.Fala = Fala;
+    }
+
+    public int getPasso(){
+        System.out.print("\nPowerRanger deu " + this.Passo);
+        return this.Passo;
+    }
+        public void setPasso(int Passo){
+            this.Passo =  Passo;
+        }
 
 }
