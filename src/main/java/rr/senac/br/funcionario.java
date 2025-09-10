7ddf26b271f374d5ab9d23724e5d176aa90d87a3
Vinicius.Laurindo <tecnicolaurindo@gmail.com>
2025-09-09 20:42:48 -0400

    commitando no intellij

--- src/main/java/rr/senac/br/funcionario.java
+++ src/main/java/rr/senac/br/funcionario.java
@@ -6,5 +6,33 @@ public class funcionario {
     public double salario;
     public String setor;
 
+
+    public String getCargo() {
+        System.out.println("Cargo: ");
+        return this.cargo = this.cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public String getMatricula() {
+        return this.matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public double getSalario() {
+        return this.salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    public String getSetor() {
+        return this.setor;
+    }
+    public void setSetor(String setor) {
+        this.setor = setor;
+    }
 }
+
 
