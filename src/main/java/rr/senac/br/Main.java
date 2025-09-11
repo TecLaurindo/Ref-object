419cc539712a75096b6808f75596ffd50ffac6a8
Vinicius.Laurindo <tecnicolaurindo@gmail.com>
2025-09-10 21:12:22 -0400

    add Scanner e forma  de usuario poder montar seu Funcionarios

--- src/main/java/rr/senac/br/Main.java
+++ src/main/java/rr/senac/br/Main.java
@@ -1,24 +1,71 @@
 package rr.senac.br;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("Digite seu gargo: ");
+        String cargo = leia.nextLine();
+
+        System.out.print("Digite sua matricula: ");
+        String matricula = leia.nextLine();
+
+        System.out.println("Digite o salario: ");
+        double salario = leia.nextDouble();
+
+        System.out.println("Digite o setor: ");
+        String setor = leia.nextLine();
+
+        System.out.println("Digite o nome: ");
+        String nome = leia.nextLine();
+
+        System.out.println("Digite a Idade: ");
+        int idade = leia.nextInt();
+
+        System.out.println("Digite o CPF: ");
+        String cpf = leia.nextLine();
+
+        System.out.println("Digite o telefone: ");
+        String telefone = leia.nextLine();
+
+        System.out.println("Digite o endereco: ");
+        String endereco = leia.nextLine();
+
+        System.out.println("Digite o data de nascimento: ");
+        String dataNascimento = leia.nextLine();
+
+        System.out.println("Digite o email: ");
+        String email = leia.nextLine();
+
+        System.out.println("Digite as Falas: ");
+        String Fala = leia.nextLine();
+
+        System.out.println("Digite a quantidade de Passos: ");
+        int Passo = leia.nextInt();
+
         Funcionario f1 = new Funcionario(
-                "T.I",
-                "55454545",
-                5000,
-                "informatica",
-                "Vinicius Laurindo",
-                18,
-                "063.853.302-30",
-                "95 984286676",
-                "no Brasil",
-                "19/05/2007",
-                "tecnicolaurindo@gmail.com",
-                "Minha pika flamejante",
-                8
+                cargo,
+                matricula,
+                salario,
+                setor,
+                nome,
+                idade,
+                cpf,
+                telefone,
+                endereco,
+                dataNascimento,
+                email,
+                Fala, Passo
         );
         System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>");
+        f1.getCargo();
 
+        f1.getSalario();
+        f1.getNome();
+        f1.getSetor();
+        f1.getTelefone();
 
         System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>");
     }
